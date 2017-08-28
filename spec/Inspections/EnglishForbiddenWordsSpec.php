bcbc4419add58d7af07fd351cfd4ca539d50f930
Denis Mitrofanov <denis.mitr@gmail.com>
2017-08-28 22:24:49 +0300

    Adds EnglishForbiddenWords inspections

---
+++ spec/Inspections/EnglishForbiddenWordsSpec.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace spec\Denismitr\Spam\Inspections;
+
+use Denismitr\Spam\Contracts\Inspection;
+use Denismitr\Spam\Exceptions\SpamDetected;
+use Denismitr\Spam\Inspections\EnglishForbiddenWords;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class EnglishForbiddenWordsSpec extends ObjectBehavior
+{
+    protected $forbidden;
+
+    public function let()
+    {
+        $this->forbidden = (new EnglishForbiddenWords)->getForbiddenWords();
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(EnglishForbiddenWords::class);
+        $this->shouldImplement(Inspection::class);
+    }
+
+    function it_detects_any_forbidden_word()
+    {
+        foreach ($this->forbidden as $word) {
+            $this
+                ->shouldThrow(SpamDetected::class)
+                ->duringDetect("Всякий текст " . $word . " ещё текст");
+        }
+    }
+
+    function it_detects_any_forbidden_word_in_uppercase()
+    {
+        foreach ($this->forbidden as $word) {
+            $word = mb_strtoupper($word);
+            $this
+                ->shouldThrow(SpamDetected::class)
+                ->duringDetect("Всякий текст " . $word . " ещё текст");
+        }
+    }
+
+    function it_detects_any_forbidden_word_as_part_of_text()
+    {
+        foreach ($this->forbidden as $word) {
+            $this
+                ->shouldThrow(SpamDetected::class)
+                ->duringDetect("Всякий текст" . $word . "ещё текст");
+        }
+    }
+
+    function it_should_not_throw_on_normal_text()
+    {
+        $this
+            ->shouldNotThrow(SpamDetected::class)
+            ->duringDetect("Всякий текст разный, и даже ещё текст");
+    }
+}
---
+++ src/Inspections/EnglishForbiddenWords.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Denismitr\Spam\Inspections;
+
+use Denismitr\Spam\Contracts\Inspection;
+use Denismitr\Spam\Exceptions\SpamDetected;
+
+class EnglishForbiddenWords implements Inspection
+{
+    protected $forbidden = [
+        'best porn',
+        'free porn',
+        'whores',
+        'fucking',
+        'fuck',
+        'shit',
+        'free sex',
+        'blow job'
+    ];
+
+    public function detect(string $text)
+    {
+        foreach ($this->getForbiddenWords() as $word) {
+            if (mb_stripos($text, $word) !== false) {
+                throw new SpamDetected("Forbidden word detected!");
+            }
+        }
+    }
+
+    public function getForbiddenWords()
+    {
+        return $this->forbidden;
+    }
+}
